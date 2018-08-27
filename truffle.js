2c761624e20f1db7c23be4597e81aaa572248449
Lisa Li <ligao22012@gmail.com>
2018-08-27 15:53:53 -0500

    update

--- truffle.js
+++ truffle.js
@@ -1,11 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
+               networks: {
+                              ganache_cli: {
+                                             host: "127.0.0.1",
+                                             port: 8545,
+                                             network_id: "*" // Match any network id
+                              },
+                              ganache_gui: {
+                                             host: "127.0.0.1",
+                                             port: 7545,
+                                             network_id: "*" // Match any network id
+                              }
+               }
+}
+
